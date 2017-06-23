af8aa353d6557ffacf397ae0c9038d07aaa94be0
Pavol Vargovcik <pallly.vargovcik@gmail.com>
2017-06-23 22:14:11 +0200

    initial

---
+++ setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='sample',
+    version='0.1.0',
+    description='Currency converter',
+    long_description=readme,
+    author='Pavol Vargovčík',
+    author_email='pavol.vargovcik@gmail.com',
+    url='https://github.com/p4l1ly/currency',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
